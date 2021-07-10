6b09172785560f4e7548a71cd61224698b320379
luchadeprogramacion1 <85137738+luchadeprogramacion1@users.noreply.github.com>
2021-07-10 22:34:52 +0200

    se crea variable Usuario, para con ese valor validar si existe, tambien generar logueo lo validamos con la contrasena hasheada y con la funcion password_verify

--- Modelos/ModeloUsuario.Class.php
+++ Modelos/ModeloUsuario.Class.php
@@ -8,49 +8,69 @@ class ModeloUsuario extends Modelo{
 	public $ApellidoUsuario;
 	public $CorreoUsuario;
 	public $ContrasenaUsuario;
-	//public $FotoPerfil;
-
+	public $FotoPerfil;
 
 	public function CrearUsuario(){
 
-		header("Location: paginaprincipal.html");
-
-		$this -> ContrasenaUsuario = $this -> hashearPassword($this -> ContrasenaUsuario);
-		$sql = "INSERT INTO usuarios(NombreUsuario,ApellidoUsuario,CorreoUsuario,ContrasenaUsuario) VALUES(
-		'{$this -> NombreUsuario}',
-		'{$this -> ApellidoUsuario}',
-		'{$this -> CorreoUsuario}',
-		'{$this -> ContrasenaUsuario}'
-		)";
+		if($this -> existeUsurario($this -> Usuario)){
+			return false;
+		}else{
+			$password = $this -> hashearPassword($this -> ContrasenaUsuario);
+			$sql = "INSERT INTO usuarios(NombreUsuario,ApellidoUsuario,CorreoUsuario,ContrasenaUsuario, usuario, FotoPerfil ) VALUES(
+			'{$this -> NombreUsuario}',
+			'{$this -> ApellidoUsuario}',
+			'{$this -> CorreoUsuario}',
+			'{$password}',
+			'{$this -> Usuario}',
+			'{$this -> FotoPerfil}'
+			)";
 	    $this -> conexion -> query($sql);
 			if($this -> conexion -> error){
-			throw new Exception("error en la creacion del usuario");
+				throw new Exception("error en la creacion del usuario");
+				return false;
+			}
+			return true;
+		}
+	}
+	
+	private function existeUsurario($Usuario){
+		
+		$sql = "SELECT count(*) as contador FROM usuarios WHERE Usuario = '{$Usuario}'";
+		$existe = $this -> conexion -> query($sql);
+		
+		foreach ($existe -> fetch_all(MYSQLI_ASSOC) as $contador ){
 
+			if ($contador['contador']>0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+ 	}
+  
+	public function generarLogueo(){
+		
+		$sql = "SELECT * FROM usuarios WHERE NombreUsuario = '{$this -> NombreUsuario}'";
+		$password = $this -> ContrasenaUsuario;
+		$contador = 0;
+		$usuario = array();
+		$query = $this -> conexion -> query($sql);
+		foreach ($query -> fetch_all(MYSQLI_ASSOC) as $usuario ){
+			$valor = password_verify($password, $usuario['ContrasenaUsuario']);
+			if (password_verify($password, $usuario['ContrasenaUsuario'])){
+				$contador++;
 			}
 		}
 
-	public function generarLogueo(){
-
-		$sql = "SELECT IdUsuario FROM usuarios WHERE NombreUsuario =
-		'{$this -> NombreUsuario}' and ContrasenaUsuario ='{$this -> ContrasenaUsuario}'";
-
-
-
-		$existe = $this -> conexion -> query($sql);		
-		$si=mysqli_num_rows($existe);
-
-		if($si === 1){
-			header("Location: paginaUsuarioLogueado.php");
-			}else{
-				echo "no existe el usuario";
+		if ($contador>0){
+			header("Location: paginaUsuarioLogueado.html");
+		}else{
+			echo "Usuario no existe o password errado";
 			}
 		}
 	
 	private function hashearPassword($ContrasenaUsuario){
+
     	return password_hash($ContrasenaUsuario,PASSWORD_DEFAULT);
     }
-
-    private function compararPasswords($passwordHasheado){
-            return password_verify($this -> password, $passwordHasheado);
-}
 }
