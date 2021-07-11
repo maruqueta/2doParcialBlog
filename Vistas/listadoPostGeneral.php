46f122fa1779b0263cfbf012eb58b63bab569a77
luchadeprogramacion1 <85137738+luchadeprogramacion1@users.noreply.github.com>
2021-07-11 06:43:31 +0200

    se crea location a pagina usauario logueado con exito.

--- Vistas/listadoPostGeneral.php
+++ Vistas/listadoPostGeneral.php
@@ -1,26 +1,26 @@
 <?php
 	require '../Controladores/ControladorPost.class.php';
 
-		if(isset($_GET["pagina"])){
+	if(isset($_GET["pagina"])){
 
-			if ($_GET["pagina"]==1){
-				header("Location:paginaprincipal.php");
-			}else{
-				$pagina=$_GET["pagina"];
-			}
+		if ($_GET["pagina"]==1){
+			header("Location:paginaprincipal.php");
 		}else{
-			$pagina=1;
+			$pagina=$_GET["pagina"];
 		}
+	}else{
+		$pagina=1;
+	}
 
-		list($InicioPagina, $PostPorPagina ,$TotalPaginas)	= ControladorPost::PaginadoPost($pagina);
+	list($InicioPagina, $PostPorPagina ,$TotalPaginas)	= ControladorPost::PaginadoPost($pagina);
 
-		foreach ( ControladorPost::ListadoPost($InicioPagina, $PostPorPagina) as $fila){
-			//echo $fila -> TituloPost . " - " . $fila -> CuerpoPost . "<br>";
-		echo "<h3>" . $fila['TituloPost'] ."</h3> ". "</a> "." - " . $fila['CuerpoPost'] ." ++++ "."<br>";
-		echo "<hr/>";
+	foreach ( ControladorPost::ListadoPost($InicioPagina, $PostPorPagina) as $fila){
+		//echo $fila -> TituloPost . " - " . $fila -> CuerpoPost . "<br>";
+	echo "<h3>" . $fila['TituloPost'] ."</h3> ". "</a> "." - " . $fila['CuerpoPost'] ." ++++ "."<br>";
+	echo "<hr/>";
 
-		}
-		
-		for($i=1;$i<=$TotalPaginas;$i++){
-				echo "<a href='?pagina=". $i ."'> ". $i . "</a>";
-		}
+	}
+	
+	for($i=1;$i<=$TotalPaginas;$i++){
+			echo "<a href='?pagina=". $i ."'> ". $i . "</a>";
+	}
