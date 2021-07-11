9e8c97899bd6c7a90cae739ba42fd2bdab0dbeb3
luchadeprogramacion1 <85137738+luchadeprogramacion1@users.noreply.github.com>
2021-07-11 06:37:42 +0200

    se crea location a pagina usauario logueado con exito.

--- Vistas/formularioRegistroPost.php
+++ Vistas/formularioRegistroPost.php
@@ -2,5 +2,7 @@
 	
 	require '../Controladores/ControladorPost.class.php';
 
-	$creado = ControladorPost::AltaPost($_POST['TituloPost'],$_POST['CuerpoPost']);
+	$creado = ControladorPost::AltaPost($_POST['TituloPost'],$_POST['CuerpoPost'], $_POST['idUsuario']);
+	
+	echo "<script>window.location= 'paginaUsuarioLogueado.php'</script>";
  	 	
