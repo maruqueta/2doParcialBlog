b511354379910d864e51b60e31d47bc2c33659a2
luchadeprogramacion1 <85137738+luchadeprogramacion1@users.noreply.github.com>
2021-07-11 15:39:17 +0200

    se agrega formulario modifica usuario y se corrige pagina moficiar usuario

--- Vistas/paginaModificarUsuario.php
+++ Vistas/paginaModificarUsuario.php
@@ -7,12 +7,12 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 <body style="background:lightblue;">
-	<?php
+			<?php
 		session_start();
 		if(!isset($_SESSION["usuario"])){
-			header("Location:index.php");
-		}
-	?>
+			header("Location:paginaprincipal.php");
+			}
+		?>
 	
 	<div class="formularioRegistro">
 		<div style="color:#ffffff;">
@@ -21,55 +21,50 @@
 			</div>&nbsp;&nbsp;
 		</div>
 		<hr>
-		<form id="formularioActualizaUsuario" action="formularioActualizaUsuario.php" method="post" enctype="multipart/form-data">
+		<form id="formularioModificaUsuario" action="formularioModificaUsuario.php" method="post" enctype="multipart/form-data">
+			<input type="hidden" name="idUsuario" id="usuario" value="<?php echo $_SESSION["usuario"]; ?>">	
 			<div class="input-group">
+          <div class="input-group-addon">
+                	<div class="glyphicon glyphicon-user">
+                	</div>
+      		</div> 
+       		<input type="text" class="form-control" name="NombreUsuario" id="nombreUsuario" maxlength="50" minlength="3" placeholder="Nombres" value = <?php echo $_SESSION["NombreUsuario"] ?> required>
+			</div>
+      <div class="input-group">
                 <div class="input-group-addon">
                 	<div class="glyphicon glyphicon-user">
                 	</div>
-                	<input type="text" class="form-control" name="NombreUsuario" id="nombreUsuario" maxlength="20" minlength="4" placeholder="Nombres" required>
-            	</div>
-	            <div class="input-group">
-	                <div class="input-group-addon">
-	                	<div class="glyphicon glyphicon-user">
-	                	</div>
-	           		</div> 
-	                <input type="text" class="form-control" name="ApellidoUsuario" id="apellidoUsuario" maxlength="20" minlength="4" placeholder="Apellidos" required>
-	            </div>
-				<div class="input-group">
-	                <div class="input-group-addon">
-	                	<div class="glyphicon glyphicon-envelope">
-	                	</div>
-	           		</div> 
-	  				<input type="email" name="CorreoUsuario" class="form-control" id="campoemail" placeholder="E-mail" required>
-				</div>
-	            <div class="input-group">
-	                <div class="input-group-addon">
-	                	<div class="glyphicon glyphicon-user">
-	                	</div>
-	           		</div> 
-	                <input type="text" class="form-control" name="Usuario" id="Usuario" maxlength="20" minlength="4" placeholder="Usuario" required>
-	            </div>
+           		</div> 
+                <input type="text" class="form-control" name="ApellidoUsuario" id="apellidoUsuario" maxlength="50" minlength="3" placeholder="Apellidos" value = <?php echo $_SESSION["ApellidoUsuario"] ?> required>
+            </div>
+			<div class="input-group">
+                	<div class="input-group-addon">
+                	<div class="glyphicon glyphicon-envelope">
+                	</div>
+           			</div> 
+  				<input type="email" name="CorreoUsuario" class="form-control" id="campoemail" placeholder="E-mail" value = <?php echo $_SESSION["CorreoUsuario"] ?>  required>
+			</div>
 			<div class="input-group">
                 <div class="input-group-addon">
                 	<div class="glyphicon glyphicon-lock">
                 	</div>
                	</div>
-                <input type="password" name="ContrasenaUsuario" class="form-control" id="clave" maxlength="20" minlength="2" required pattern="[A-Za-z0-9]+" placeholder="Contraseña" required>
+                <input type="password" name="ContrasenaUsuario" class="form-control" id="clave" maxlength="20" minlength="2" pattern="[A-Za-z0-9]+" placeholder="Ingrese nueva Contrasenia o deje vacio">
             </div>
-            <div>
 				<input type="hidden" name="MAX_TAM" value="2097152">
 				<label>Selecionar Foto de Perfil</label>
 				</br>Selecciona una imagen con tamaño inferior a 2 MB
 				<input type="file" name="FotoPerfil" id="FotoPerfil">
-			</div>
-			<div class="form-group">
-  				<button type="submit" class="btn btn-default" onclick="location.href='paginaUsuarioLogueado.php';">Actualizar
-  				</button>
+					<div class="form-group">
+							<button type="submit" class="btn btn-default" onclick="location.href='paginaUsuarioLogueado.php';">Actualizar
+			  				</button>
+  				</div>
   				 </form>
-            	<button type="submit" class="btn btn-default" onclick="location.href='paginaUsuarioLogueado.php';">Cancelar
-            	</button> 
-            </div>
-     	</div>
+  		
+  		<div class="form-group">
+  			<button type="submit" class="btn btn-default" onclick="location.href='paginaUsuarioLogueado.php';">Cancelar
+            	</button>
+  			</div>
 	</div>
 </body>
 </html>
